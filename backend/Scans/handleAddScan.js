e1c98965ad00fac881547a55f3e8662c12feaa52
AnthonyL103 <143826886+AnthonyL103@users.noreply.github.com>
2025-02-14 15:36:41 -0800

    added more backend bug fixes

--- backend/Scans/handleAddScan.js
+++ backend/Scans/handleAddScan.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const { parseTextFromBuffer, parseTextFromPDF } = require('../parse');
+const { parseTextFromBuffer, parseTextFromPDF } = require('./parse');
 const { userinfos, ParsedTextEntries } = require('../Database/db');
 
 const { v4: uuidv4 } = require('uuid');
