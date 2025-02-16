e639fab0d09c575c8c3c6168729ee1bbfae578af
AnthonyL103 <143826886+AnthonyL103@users.noreply.github.com>
2025-02-16 14:23:08 -0800

    added small debugging statements

--- backend/Authentication/handleAccount.js
+++ backend/Authentication/handleAccount.js
@@ -15,6 +15,7 @@ sgMail.setApiKey(process.env.MAILERKEY);
 
 async function sendVerificationEmail(email, verificationLink) {
     try {
+        console.log(MAILERKEY, EMAIL_FROM);
         const msg = {
             to: email,
             from: process.env.EMAIL_FROM, // Must be a verified sender in SendGrid
