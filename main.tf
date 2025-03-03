aa0c715a0a5ccd6af6a7ba059f7dd03735463611
Anthony Li <anthonyli0330@gmail.com>
2025-03-02 17:12:51 -0800

    added imports for existing resources

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"  
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2"  # Your region
 }
 
 
