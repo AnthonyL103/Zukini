200443885e289db2232af460005d66566b80aa8f
Anthony Li <anthonyli0330@gmail.com>
2025-03-23 23:25:05 -0700

    added premium badge for prem users

--- frontendv2/src/components/Header/Navbar.jsx
+++ frontendv2/src/components/Header/Navbar.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation} from 'react-router-dom';
 import { useUser } from '../authentication/UserContext';
 import { v4 as uuidv4 } from 'uuid';
 import { Settings } from 'lucide-react';
 
 const Navbar = () => {
       
-  const navigate = useNavigate();
-  const { userId, setUserId, setEmail, setTotalScans, setTotalFlashcards, setTotalMockTests, setName, setisforgot} = useUser();
+  const { userId, setUserId, setEmail, setTotalScans, setTotalFlashcards, setTotalMockTests, setName, setisforgot, isPremium} = useUser();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   
   const handleLogout = () => {
@@ -151,7 +150,34 @@ useEffect(() => {
           <div className={`hidden md:flex md:items-center  ${isHomePage ? 'bg-transparent' : 'bg-none'}`}>
           {!isGuestUser ? (
               <>
-                
+                {isPremium && (
+                <div className="flex items-center gap-1 text-yellow-600">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.8"
+                    className="w-6 h-6 stroke-yellow-600"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                    ></path>
+                  </svg>
+                  <span 
+                    className="font-bold bg-gradient-to-r from-yellow-600 via-yellow-400 to-yellow-400 text-transparent bg-clip-text bg-[length:200%_auto] group-hover:animate-[shimmer_2s_linear_infinite]"
+                    style={{
+                      '--shimmer-from': '0%',
+                      '--shimmer-to': '200%'
+                    }}
+                  >
+                    Premium
+                  </span>
+                </div>
+              )}
+                              
+
                 <Link 
                   to="/account" 
                   className={`p-2 rounded-full hover:bg-white/10 transition-colors ${isHomePage ? 'text-white hover:text-indigo-200' : 'text-white hover:text-indigo-200'}`}
@@ -208,7 +234,32 @@ useEffect(() => {
           
           {!isGuestUser ? (
             <>
-                  
+              {isPremium && (
+                <div className="flex items-center gap-1 text-yellow-600">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.8"
+                    className="w-6 h-6 stroke-yellow-600"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                    ></path>
+                  </svg>
+                  <span 
+                    className="font-bold bg-gradient-to-r from-yellow-600 via-yellow-400 to-yellow-400 text-transparent bg-clip-text bg-[length:200%_auto] group-hover:animate-[shimmer_2s_linear_infinite]"
+                    style={{
+                      '--shimmer-from': '0%',
+                      '--shimmer-to': '200%'
+                    }}
+                  >
+                    Premium
+                  </span>
+                </div>
+              )}
                   
                
               <Link
